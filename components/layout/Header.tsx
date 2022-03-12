0b1a3558cd527e03c32da73735680d183eb7c3f9
Marco <marco.fura@unmsm.edu.pe>
2022-03-12 12:00:24 -0500

    first release

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -2,16 +2,24 @@ import Link from "next/link";
 import { useState } from "react";
 import {RiMenu5Fill} from "react-icons/ri";
 import {AiOutlineArrowUp} from "react-icons/ai";
+import {BiCurrentLocation} from "react-icons/bi";
+import { NextRouter, useRouter } from "next/router";
 export default function Header(){
     const [show,setShow]=useState<boolean>(false);
+    const {pathname}:NextRouter=useRouter();
     return(
-        <header className="sticky flex items-center justify-between top-0 py-4 px-8 bg-gray-200 text-gray-800" >
-            <h1 className="text-2xl font-bold tracking-widest " >Finder</h1>
-            <nav>
-                <button onClick={()=>show?setShow(false):setShow(true)} className="flex justify-center items-center text-3xl text-purple-500">
+        <header className="sticky z-20 flex items-center justify-between top-0 2xl:py-6 2xl:px-12 py-4 px-8 bg-gray-200 text-gray-800" >
+            <article className="flex" >
+            <span className="flex text-2xl 2xl:text-4xl text-purple-700 justify-center items-center" >
+                <BiCurrentLocation />
+            </span>
+            <h1 className="text-2xl 2xl:text-4xl font-bold tracking-widest " >Finder</h1>
+            </article>
+            <nav className="flex" >
+                <button onClick={()=>show?setShow(false):setShow(true)} className="flex xl:hidden justify-center 2xl:text-4xl items-center text-3xl text-purple-500">
                     <RiMenu5Fill/>
                 </button>
-                <aside className={`fixed py-4 flex items-center flex-col left-0 top-0 right-0 z-30 bg-purple-500 transition-transform duration-500 ${show?"translate-y-0":"-translate-y-full"}`} >
+                <aside className={`fixed py-4 flex items-center flex-col left-0 top-0 right-0 z-50 bg-purple-500 transition-transform duration-500 ${show?"translate-y-0":"-translate-y-full"}`} >
                     <article>
                         <button onClick={()=>show?setShow(false):setShow(true)} className="rounded-full transition-colors duration-500 text-white hover:bg-white hover:text-purple-500 p-4 text-2xl">
                             <AiOutlineArrowUp/>
@@ -33,25 +41,51 @@ export default function Header(){
                         <a className=" transition-all duration-300 hover:tracking-widest" >Servicios</a>
                         </Link>
                     </li>
+                    <li className="py-8" >
+                        <Link href="/" >
+                            <a className="border-2 font-bold rounded-lg transition-all duration-300 text-sm border-white py-3 px-6 hover:bg-white uppercase hover:text-purple-500" >
+                                Iniciar sesión
+                            </a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/" >
+                            <a className=" bg-gray-700 rounded-lg font-bold text-white py-3 px-6 uppercase text-sm " >Registrarse</a>
+                        </Link>
+                    </li>
                     </ul>
                 </aside>
-                <ul className="hidden " >
-                    <li>
+                <ul className="hidden xl:flex 2xl:text-2xl" >
+                    <li className="mx-4" >
                         <Link href="/" >
-                        <a>Inicio</a>
+                        <a className={`hover:text-purple-500 duration-300 transition-all hover:font-bold ${pathname.split("/").pop()===""&&"text-purple-500 font-bold"}`} >Inicio</a>
                         </Link>
                     </li>
-                    <li>
+                    <li className="mx-4" >
                         <Link href="/" >
-                        <a>Acerca de Finder</a>
+                        <a className="hover:text-purple-500 duration-300 transition-all hover:font-bold" >Acerca de Finder</a>
                         </Link>
                     </li>
-                    <li>
+                    <li className="mx-4" >
                         <Link href="/" >
-                        <a>Servicios</a>
+                        <a className="hover:text-purple-500 duration-300 transition-all hover:font-bold" >Servicios</a>
                         </Link>
                     </li>
                 </ul>
+                <nav className="hidden xl:flex  " >
+                    <article className="mx-4 ">
+                        <Link href="/" >
+                            <a className="border-2  font-bold rounded-lg transition-all duration-300 text-xs text-purple-500 2xl:text-lg border-purple-500 py-3 px-6 hover:bg-purple-500 uppercase hover:text-white" >
+                                Iniciar sesión
+                            </a>
+                        </Link>
+                    </article>
+                    <article className="mx-4" >
+                        <Link href="/" >
+                            <a className=" bg-gray-700 rounded-lg font-bold text-white py-3 px-6 uppercase text-xs 2xl:text-lg " >Registrarse</a>
+                        </Link>
+                    </article>
+                </nav>
             </nav>
         </header>
     )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,39 @@
 import { ReactElement } from "react";
 import Layout from "../components/layout";
 import {BsSearch} from "react-icons/bs";
+import {IoIosArrowDown} from "react-icons/io";
+import Link from "next/link";
 export default function Home(){
     return(
-        <section className="p-4 flex justify-center flex-col text-white items-center min-h-screen bg-gradient-to-br from-purple-800 to-purple-500 " >
-            <p className="uppercase tracking-widest" >Uso gratuito</p>
-            <h1 className="text-3xl w-48 font-semibold text-center" >
+        <section className="p-4 flex justify-center flex-col text-white items-center min-h-screen bg-gradient-to-br from-purple-900 to-purple-400  " >
+            <p className="uppercase tracking-widest 2xl:text-xl " >Uso gratuito</p>
+            <h1 className="text-3xl md:text-4xl 2xl:text-7xl 2xl:w-96 lg:text-5xl lg:w-60 w-48 font-semibold text-center" >
                 Encuentra cualquier lugar en 1 minuto
             </h1>
-            <form className="my-4" >
-                <article className="relative" >
-                    <input className=" w-80 rounded-3xl p-4 text-gray-800"  type="search" placeholder="Ej: Vidrieria San Carlos" />
-                    <span className="flex text-2xl top-4 right-4 bg-white text-gray-700 absolute justify-center items-center" >
+            <form className="my-4 w-full sm:w-96 2xl:w-[40rem] lg:w-[28rem] " >
+                <article className="relative " >
+                    <input className=" w-full 2xl:text-2xl rounded-3xl 2xl:rounded-full 2xl:p-8  p-4 z-0 text-gray-800"  type="search" placeholder="Ej: Vidrieria San Carlos" />
+                    <span className="flex text-2xl 2xl:text-4xl top-4 right-4 2xl:top-7 2xl:right-6 bg-white text-gray-700 absolute justify-center items-center" >
                         <BsSearch />
                     </span>
                 </article>
-                <article className="flex justify-center py-4" >
-                    <button className="bg-gray-800 py-2 px-4 w-28 text-sm font-semibold uppercase flex justify-center  rounded-xl transition-all duration-500 tracking-widest hover:bg-white hover:text-purple-500" type="submit" >Buscar</button>
+                <article className="flex justify-center py-6" >
+                    <button className="bg-gray-800 hover:-translate-y-2 hover:opacity-95 hover:shadow-xl py-3 px-6 2xl:py-4 2xl:px-8 2xl:text-xl  text-sm font-semibold uppercase flex justify-center  rounded-2xl transition-all duration-500 tracking-widest hover:bg-white hover:text-purple-500" type="submit" >Buscar</button>
                 </article>
             </form>
+            <article className="flex justify-center" >
+                <span className="flex text-3xl animate-bounce justify-center items-center" >
+                    <IoIosArrowDown/>
+                </span>
+            </article>
+            <article className="flex flex-col items-center" >
+                <p className="text-xl 2xl:text-2xl my-4 text-center" >¿Deseas agregar una nueva dirección?</p>
+                <Link href="/" >
+                    <a className="py-3 px-6 2xl:py-4 2xl:px-8 2xl:text-xl rounded-2xl uppercase tracking-widest border-2 border-white text-white font-bold hover:bg-white transition-all duration-300 hover:text-purple-500" >
+                        Comenzar
+                    </a>
+                </Link>
+            </article>
         </section>
     )
 }
